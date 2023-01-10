8672fa595b119827164f620dc444ad772df928f7
wesleyfarias <wesleybezerra3979@gmail.com>
2023-01-10 18:34:28 -0300

    varios entes

--- controllers/user/entesController.js
+++ controllers/user/entesController.js
@@ -1,4 +1,18 @@
 class entesController {
+  async viewentesId(req, res) {
+    switch (req.params.id) {
+      case '1':
+        res.render("user/entes/entes1Page");
+        break;
+      case '2':
+        res.render("user/entes/entes2Page");
+        break;
+      default:
+        res.redirect("/entes");
+        break;
+    }
+  }
+
   async viewentes(req, res) {
     res.render("user/entes/entesPage");
   }
